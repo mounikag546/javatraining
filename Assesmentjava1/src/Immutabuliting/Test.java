4ef4010a474aa97061a950d72ef1f9f0cb139af5
mounika <mounikag546@gmail.com>
2022-05-17 17:35:20 +0530

    initial commit

---
+++ Assesmentjava1/src/Immutabuliting/Test.java
@@ -0,0 +1,22 @@
+package Immutabuliting;
+
+public class Test {
+    private String name;
+    private String address;
+
+    public String getName() {
+        return name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public String getSalary() {
+        return salary;
+    }
+
+    private String salary;
+
+}
+
